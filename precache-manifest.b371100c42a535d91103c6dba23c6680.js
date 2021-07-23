9e3b60ca4e269eb8981002f35b9f5cbb56a63761
Wouter van den Heuvel <exo_00@hotmail.com>
2021-07-23 12:42:26 +0200

    Updates

---
+++ precache-manifest.b371100c42a535d91103c6dba23c6680.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c85a5521aee7060fa87f749a4875a925",
+    "url": "./index.html"
+  },
+  {
+    "revision": "89ff152ce1f001628027",
+    "url": "./static/css/main.9f086ad5.chunk.css"
+  },
+  {
+    "revision": "89f433aa03279c26d9d5",
+    "url": "./static/js/2.8ad009e6.chunk.js"
+  },
+  {
+    "revision": "c5db7362081f53c57da384eb724af515",
+    "url": "./static/js/2.8ad009e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89ff152ce1f001628027",
+    "url": "./static/js/main.200f9eb2.chunk.js"
+  },
+  {
+    "revision": "7fa58438b4a17459b297",
+    "url": "./static/js/runtime-main.4752a450.js"
+  },
+  {
+    "revision": "81786b3c0eb5c4e32f4a549579df8997",
+    "url": "./static/media/check.81786b3c.svg"
+  },
+  {
+    "revision": "7ec0d58f39483cae3651388e18dfb00c",
+    "url": "./static/media/close.7ec0d58f.svg"
+  }
+]);
