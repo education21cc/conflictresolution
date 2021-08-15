df49ccd18607ffc96cf7064c6983acc2777da199
Wouter van den Heuvel <exo_00@hotmail.com>
2021-08-15 11:26:11 +0200

    added MS and CH

--- src/App.tsx
+++ src/App.tsx
@@ -47,9 +47,10 @@ function App() {
         // @ts-ignore
         
 
-        fetch(`${process.env.PUBLIC_URL}/config/conflict-resolution-CH.json`)
+        // fetch(`${process.env.PUBLIC_URL}/config/conflict-resolution-MS.json`)
         // fetch(`${process.env.PUBLIC_URL}/config/conflict-resolution-NL.json`)
         // fetch(`${process.env.PUBLIC_URL}/config/conflict-resolution-EN.json`)
+        fetch(`${process.env.PUBLIC_URL}/config/conflict-resolution-CH.json`)
         .then((response) => {
           response.json().then((data) => {
 
